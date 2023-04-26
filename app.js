f03080a01c7ecee444638a0d803530b03a136fb1
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-04-26 06:02:10 +0200

    Tik Tak Toe

--- app.js
+++ app.js
@@ -4,5 +4,42 @@
 ====================================
 ***********************************/
 
+// select table td elements
+const tableItems = document.querySelectorAll('.game tbody .row .column');
 
+
+// global object saves x and o elements
+const xAndO = new Object();
+
+Object.defineProperties(xAndO, {
+  x: {
+    writable: false,
+    enumerable: true,
+    configurable: false,
+    value: `<div class="x">
+    <div class="left-cross"></div>
+    <div class="right-cross"></div>
+  </div>`,
+  },
+  o: {
+    writable: false,
+    enumerable: true,
+    configurable: false,
+    value: `<svg class="o">
+    <circle cx="28" cy="28" r="28"></circle>
+  </svg>`,
+  }
+});
+
+// define the game main class
+class TikTakToe {
+
+}
+
+// play game when click on any item
+tableItems.forEach(ele => {
+  ele.addEventListener('click', () => {
+    console.log(ele.getAttribute('_id'));
+  });
+});
 
